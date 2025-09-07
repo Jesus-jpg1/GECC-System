9256be34c436d49aa4a672514d687bf33753f5a1
Guilherme de Jesus Carvalho <guilhermejesusac852@gmail.com>
2025-09-07 12:58:37 -0500

    feat(admin): Registrar modelos na interface de administração do Django

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ServidorProfile, Edital, Atividade, LancamentoHoras
 
-# Register your models here.
+# Registrando os modelos para que apareçam na interface de administração
+admin.site.register(ServidorProfile)
+admin.site.register(Edital)
+admin.site.register(Atividade)
+admin.site.register(LancamentoHoras)
