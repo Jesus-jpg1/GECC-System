6dfee7f1fa4ab8bee281ed8e6c0b6a2e3e5453f7
Guilherme de Jesus Carvalho <guilhermejesusac852@gmail.com>
2025-09-07 19:01:08 -0500

    feat(edital): Cria a listagem de editais para a Unidade Demandante

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Nossa URL principal do painel (ex: http://127.0.0.1:8000/painel/)
     path('painel/', views.painel, name='painel'),
+    path('editais/', views.listar_editais, name='listar_editais'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 # core/views.py
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Edital
 
 @login_required
 def painel(request):    
@@ -17,3 +18,15 @@ def painel(request):
         return render(request, 'painel_prodgep.html')
     else:
         return render(request, 'painel_padrao.html')
+    
+@login_required
+def listar_editais(request):
+    if request.user.servidorprofile.funcao != 'Unidade Demandante':
+        return redirect('painel')
+    
+    editais = Edital.objects.filter(criado_por=request.user).order_by('-created_at')
+
+    context = {
+        'editais': editais
+    }
+    return render(request, 'listar_editais.html', context)
