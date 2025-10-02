2a4150ac83269049a871e478719a78f7bee27eff
Guilherme de Jesus Carvalho <guilhermejesusac852@gmail.com>
2025-10-01 23:43:44 -0500

    feat: Configura PostgreSQL para produção

--- geccsystem/settings.py
+++ geccsystem/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 import os
 
@@ -82,12 +83,19 @@ WSGI_APPLICATION = "geccsystem.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if 'DATABASE_URL' in os.environ:
+    # Configuração para produção (Render)
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
-}
+else:
+    # Configuração para desenvolvimento (sua máquina local)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
